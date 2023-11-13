d5384b705bd993651fc2ec91c44952a54e75eaa4
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-11-13 23:18:24 +0100

    create book model

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 
@@ -8,6 +9,17 @@ class Auther(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     biography = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    auther = models.ForeignKey(Auther, on_delete=models.CASCADE, related_name='book_auther')
+    publish_date = models.DateTimeField(default=timezone.now)
+    price = models.FloatField()
+
 
     def __str__(self):
         return self.name
