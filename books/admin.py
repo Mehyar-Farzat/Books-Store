ef304acd133a6867316561aac85de6bb89861595
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-11-13 23:30:07 +0100

    register all models in admin.py

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Auther, Book, Review
 
 # Register your models here.
+
+admin.site.register(Auther)
+admin.site.register(Book)
+admin.site.register(Review)
