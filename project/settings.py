dc8007bdf004a53baabed8f20aaa95955c41223f
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-03 20:32:29 +0100

    add Yet Swagger generator to test api

--- project/settings.py
+++ project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
-    'django_celery_results',
+    'drf_yasg',
+    
 
     # my apps
     'books',
